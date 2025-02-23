cb003f4c0caf87cee07c14d8df972720119d3215
SteynMollie <steyn.janus@mollie.com>
2025-02-23 15:45:59 +0100

    add webshopbuilderapp as non-submodule

---
+++ WebshopBuilderApp/screens/WelcomeScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Stylesheet, View, Text, Button} from 'react-native';
+
+export default function WelcomeScreen({ navigation }) { // Get navigation prop
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Welcome to SJ-solutions Webshop Builder App!</Text>
+            <Button
+                title="Login"
+                onPress={() => navigation.navigate('Main')} //Navigate to main screen
+            />
+        </View>
+    );
+}
+
+const styles = Stylesheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+});
+
